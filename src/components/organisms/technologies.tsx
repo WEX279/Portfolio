ed89176edf28cfecb15d0833be1b795a1eb89972
WEX279 <legazyuri279@gmail.com>
2026-08-02 20:19:37 +0200

    refactor technologies responsive

--- src/components/organisms/technologies.tsx
+++ src/components/organisms/technologies.tsx
@@ -12,62 +12,64 @@ import { MongoIcon } from "../atoms/MongoIcon"
 function Technologies(){
     return(
         <div>
-            <div className="flex gap-1.5 p-2 flex-col items-center">  
-                <p className="font-bold text-[1.5em]">Technologies</p>
-                <div className="border rounded-xl ">
-                    <p className="flex justify-center font-bold p-1">Development sources</p>
-                    <div className="flex">
-                        <div >
-                            <GitIcon/>
+            <div className="flex p-2 flex-col items-center">  
+                <p className="font-bold text-[1.5em] p-[0.5em]">Technologies</p>
+                <div className="flex flex-col md:flex md:flex-row md:gap-[1.5em]">
+                    <div className="border rounded-xl">
+                        <p className="flex justify-center font-bold p-1">Development sources</p>
+                        <div className="flex">
+                            <div >
+                                <GitIcon/>
+                            </div>
+                            <div>
+                                <img className="w-[5em] m-[1em]" src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png"/>
+                            </div>
                         </div>
-                        <div>
-                            <img className="w-[5em] m-[1em]" src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png"/>
+                        <div className="flex justify-center">
+                            <div>
+                                <VscodeIcon/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex justify-center">
-                        <div>
-                            <VscodeIcon/>
-                        </div>
-                    </div>
-                </div>  
+                    </div>  
 
-                <div className="border rounded-xl">
-                    <p className="flex justify-center font-bold">Frontend engines</p>
-                    <div className="flex">
-                        <div>
-                            <HtmlIcon/>
+                    <div className="border rounded-xl">
+                        <p className="flex justify-center font-bold">Frontend engines</p>
+                        <div className="flex">
+                            <div>
+                                <HtmlIcon/>
+                            </div>
+                            <div>
+                                <CssIcon/>
+                            </div>
                         </div>
-                        <div>
-                            <CssIcon/>
+                        <div className="flex">
+                            <div>
+                                <TailwindIcon/>
+                            </div>
+                            <div>
+                                <JsIcon/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex">
-                        <div>
-                            <TailwindIcon/>
-                        </div>
-                        <div>
-                            <JsIcon/>
-                        </div>
-                    </div>
-                    <div className="flex justify-center">
-                        <div>
-                            <ReactIcon/>
-                        </div>
-                    </div>
-                </div>
-                <div className="border rounded-xl">
-                    <p className="flex justify-center font-bold">Backend engines</p>
-                    <div className="flex">
-                        <div>
-                            <NodeIcon/>
-                        </div>
-                        <div>
-                            <ExpressIcon/>
+                        <div className="flex justify-center">
+                            <div>
+                                <ReactIcon/>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex justify-center">
-                        <div>
-                            <MongoIcon/>
+                    <div className="border rounded-xl">
+                        <p className="flex justify-center font-bold">Backend engines</p>
+                        <div className="flex">
+                            <div>
+                                <NodeIcon/>
+                            </div>
+                            <div>
+                                <ExpressIcon/>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div>
+                                <MongoIcon/>
+                            </div>
                         </div>
                     </div>
                 </div>
