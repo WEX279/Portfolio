3d067a113152316e9b4047166cc4c2ba3395fb9c
WEX279 <legazyuri279@gmail.com>
2026-08-02 17:45:30 +0200

    refactor tech structure and add borders to types

--- src/components/organisms/technologies.tsx
+++ src/components/organisms/technologies.tsx
@@ -1,4 +1,5 @@
 import { GitIcon } from "../atoms/GitIcon"
+import { VscodeIcon } from "../atoms/VscodeIcon"
 import { HtmlIcon } from "../atoms/HtmlIcon"
 import { CssIcon } from "../atoms/CssIcon"
 import { TailwindIcon } from "../atoms/TailwindIcon"
@@ -11,33 +12,48 @@ import { MongoIcon } from "../atoms/MongoIcon"
 function Technologies(){
     return(
         <div>
-            <div className="flex gap-1.5 p-2">    
-                <div >
-                    <GitIcon/>
+            <div className="flex gap-1.5 p-2 flex-col items-center">  
+                <p className="font-bold text">Technologies</p>
+                <div className="border rounded-xl p-[1em]">
+                    <p></p>
+                    <div >
+                        <GitIcon/>
+                    </div>
+                    <div>
+                        <img src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png" className="w-[5em]"/>
+                    </div>
+                    <div>
+                        <VscodeIcon/>
+                    </div>
+                </div>  
+
+                <div className="border rounded-xl">
+                    <div>
+                        <HtmlIcon/>
+                    </div>
+                    <div>
+                        <CssIcon/>
+                    </div>
+                    <div>
+                        <TailwindIcon/>
+                    </div>
+                    <div>
+                        <JsIcon/>
+                    </div>
+                    <div>
+                        <ReactIcon/>
+                    </div>
                 </div>
-                <div>
-                    <HtmlIcon/>
-                </div>
-                <div>
-                    <CssIcon/>
-                </div>
-                <div>
-                    <TailwindIcon/>
-                </div>
-                <div>
-                    <JsIcon/>
-                </div>
-                <div>
-                    <ReactIcon/>
-                </div>
-                <div>
-                    <NodeIcon/>
-                </div>
-                <div>
-                    <ExpressIcon/>
-                </div>
-                <div>
-                    <MongoIcon/>
+                <div className="border rounded-xl p-[1em]">
+                    <div>
+                        <NodeIcon/>
+                    </div>
+                    <div>
+                        <ExpressIcon/>
+                    </div>
+                    <div>
+                        <MongoIcon/>
+                    </div>
                 </div>
             </div>
         </div>
