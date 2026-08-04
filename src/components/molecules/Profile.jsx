b43e0c62caf6fc1dc3e6fd1160f94db8b300e6e2
WEX279 <legazyuri279@gmail.com>
2026-08-04 18:39:41 +0200

    feat add linkedin and github icons

--- src/components/molecules/Profile.jsx
+++ src/components/molecules/Profile.jsx
@@ -1,16 +1,23 @@
 import kirk from "../../assets/kirk.jpg"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 function Profile(){
     return(
-        <div className="flex flex-col ">
-            <div className="flex justify-center">
-                <img src={kirk} className="rounded-full w-[10em] p-2"/>
-            </div>
-            <div className="flex flex-col">
-                <h1 className="self-center font-bold text-[1.5em]">Yuri Legaz Brasil</h1>
-                <div className="self-center flex-wrap">
-                    <p>✉ legazyuri279@gmail.com</p>
-                    <p>☎ 612 20 82 45 | 🌏︎ Spain</p>
+        <div className="flex flex-col">
+            <div className="md:flex md:flex-row-reverse md:flex md:justify-around">
+                <div className="flex justify-center ">
+                    <img src={kirk} className="rounded-full w-[10em] p-2 md:w-[30em] "/>
+                </div>
+                <div className="flex flex-col md:flex md:self-center">
+                    <h1 className="self-center font-bold text-[1.5em]">Yuri Legaz Brasil</h1>
+                    <div className="self-center flex-wrap p-1">
+                        <p>✉ legazyuri279@gmail.com</p>
+                        <p>☎ 612 20 82 45</p>
+                    </div>
+                    <div className="flex justify-center gap-2">
+                        <FaGithub className="w-10 h-10 text-black hover:text-slate transition-colors"/>
+                        <FaLinkedin className="w-10 h-10 text-black hover:text-sky-600 transition-colors"/>
+                    </div>
                 </div>
             </div>
             <div>
