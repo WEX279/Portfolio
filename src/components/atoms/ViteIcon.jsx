d84d556055f822763047ea3f52c2f35b893f2d68
WEX279 <legazyuri279@gmail.com>
2026-08-06 19:43:23 +0200

    feat add vercel && vite icons

---
+++ src/components/atoms/ViteIcon.jsx
@@ -0,0 +1,9 @@
+function ViteIcon(){
+    return(
+        <img className="w-[2.5em] m-[1em] md:w-[4em]" src="https://icon.icepanel.io/Technology/svg/Vite.js.svg"/>
+    )
+}
+
+export {
+    ViteIcon
+}
