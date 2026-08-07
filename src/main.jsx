80ac8f3d01e0c2da5ef072a24fc66e59e561c7f4
WEX279 <legazyuri279@gmail.com>
2026-08-07 19:05:55 +0200

    fix localhost connection

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter 
+    // basename={import.meta.env.BASE_URL}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/'
+  // base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/'
 })
