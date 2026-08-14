b76e66873a6cec05d0612d46fa74515d28de77ea
WEX279 <legazyuri279@gmail.com>
2026-08-14 20:19:33 +0200

    add msync project

--- src/components/molecules/MSmini.jsx
+++ src/components/molecules/MSmini.jsx
@@ -3,9 +3,12 @@ import MSthumbnail from '../../assets/mangaSyncThumbnail.png'
 function MSmini(){
     return(
         <div>
+            <div className="rounded-xl border-black border-2 w-3xs">
             <a href="https://manga-react-rouge.vercel.app/" target="blank">
-                <img src={MSthumbnail} className="flex rounded-t"/>
+                <img src={MSthumbnail} className="flex rounded-t-xl w-3xs"/>
+                <p>Developed with JavaScript, MongoDB, Express in backend and React, Vite, Tailwind in frontend using atomic design. </p>
             </a>
+            </div>
         </div>
     )
 }
