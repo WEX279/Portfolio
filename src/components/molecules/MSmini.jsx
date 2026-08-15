4ab198973f1dd60eb6d68650f3087582e5a2f1f3
WEX279 <legazyuri279@gmail.com>
2026-08-15 18:47:41 +0200

    refactor ms desc

--- src/components/molecules/MSmini.jsx
+++ src/components/molecules/MSmini.jsx
@@ -7,7 +7,11 @@ function MSmini(){
             <div className="rounded-xl border-black border-2 w-3xs">
             <a href="https://manga-react-rouge.vercel.app/" target="blank">
                 <img src={MSthumbnail} className="flex rounded-t-xl w-3xs"/>
-                <p>Developed with JavaScript, MongoDB, Express in backend and React, Vite, Tailwind in frontend using atomic design. </p>
+                <div className="p-[0.5em]">
+                        <p>Manga platform with secure user auth (Bcrypt/JWT) and detailed manga cards fetched asynchronously from a custom backend.</p>
+                        <p>Developed with JavaScript, MongoDB, Express in backend and React, Vite, Tailwind in frontend using atomic design. </p>
+                    </div>
+                
             </a>
             </div>
         </div>
