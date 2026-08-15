d923774f9c490c4265484ac65b1e10c03409b2df
WEX279 <legazyuri279@gmail.com>
2026-08-15 18:54:35 +0200

    refactor text to fix better

--- src/components/molecules/MSmini.jsx
+++ src/components/molecules/MSmini.jsx
@@ -8,8 +8,8 @@ function MSmini(){
             <a href="https://manga-react-rouge.vercel.app/" target="blank">
                 <img src={MSthumbnail} className="flex rounded-t-xl w-3xs"/>
                 <div className="p-[0.5em]">
-                        <p>Manga platform with secure user auth (Bcrypt/JWT) and detailed manga cards fetched asynchronously from a custom backend.</p>
-                        <p>Developed with JavaScript, MongoDB, Express in backend and React, Vite, Tailwind in frontend using atomic design. </p>
+                        <p>Manga platform with user auth (Bcrypt/JWT) and manga cards fetched from a custom backend.</p>
+                        <p>Developed with JS, MongoDB, Express in backend and React, Vite, Tailwind in frontend. </p>
                     </div>
                 
             </a>
