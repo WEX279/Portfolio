090899d04c9593a90e3a88d3ad5a80a3ad493294
WEX279 <legazyuri279@gmail.com>
2026-08-15 18:38:51 +0200

    fix description

--- src/components/molecules/RMAmini.jsx
+++ src/components/molecules/RMAmini.jsx
@@ -6,7 +6,10 @@ function RMAmini(){
             <div className="rounded-xl border-black border-2 w-3xs">
                 <a href="https://wex279.github.io/Rick-MortyAPI/" target="blank">
                     <img src={RMmini} className="flex rounded-t-xl w-3xs"/>
-                    <p>Developed with JavaScript, MongoDB, Express in backend and React, Vite, Tailwind in frontend using atomic design. </p>
+                    <div className="p-[0.5em]">
+                        <p>A web page where you can fetch information about characters as well as their details, such as status, genre or specie.</p>
+                        <p>Developed with HTML5, CSS and JavaScript.</p>
+                    </div>
                 </a>
             </div>
         </div>
