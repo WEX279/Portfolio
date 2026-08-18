002e1de17ad5213fa4553257619123832b011b12
WEX279 <legazyuri279@gmail.com>
2026-08-18 19:23:28 +0200

    refactor responsive (mobile)

--- src/components/organisms/Titles.jsx
+++ src/components/organisms/Titles.jsx
@@ -6,13 +6,19 @@ import m4 from '../../assets/titles/Frontend_con_React_-.png'
 function Titles(){
     return(
         <div>
-            <div className="p-[1em]">
-                <p className="font-bold flex justify-center text-[2em]">Titles</p>
-                <div className="flex justify-around">
-                    <img className=" w-[10em] " src={m1} alt="Fundamentos_Web_-_(HTML,_CSS,_JavaScript_y_Github)"/>
-                    <img className=" w-[10em] " src={m2} alt="JavaScript_Avanzado, _Asincronía_y_Visualización_"/>
-                    <img className=" w-[10em] " src={m3} alt="Backend_con_Node.js,_Express_y_Bases_de_Datos_"/>
-                    <img className=" w-[10em] " src={m4} alt="Frontend_con_React_-"/>
+            <div className="p-[1em] flex justify-center flex-col">
+                <p className="flex justify-center font-bold text-[2em] pb-[0.5em]">Titles</p>
+                <div className="flex self-center flex-col md:justify-around">
+
+                    <div className="flex">
+                        <img className=" w-[10em] " src={m1} alt="Fundamentos_Web_-_(HTML,_CSS,_JavaScript_y_Github)"/>
+                        <img className=" w-[10em] " src={m2} alt="JavaScript_Avanzado, _Asincronía_y_Visualización_"/>
+                    </div>
+
+                    <div className="flex">
+                        <img className=" w-[10em] " src={m3} alt="Backend_con_Node.js,_Express_y_Bases_de_Datos_"/>
+                        <img className=" w-[10em] " src={m4} alt="Frontend_con_React_-"/>
+                    </div>
                 </div>
             </div>
         </div>
