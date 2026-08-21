bc64ecd366aaa76148b8caff7ae3a63a3e264637
WEX279 <legazyuri279@gmail.com>
2026-08-21 23:22:12 +0200

    refactor techs structure for responsive plus carrousel

--- src/components/organisms/technologies.jsx
+++ src/components/organisms/technologies.jsx
@@ -13,38 +13,66 @@ import { ViteIcon } from "../atoms/ViteIcon"
 
 function Technologies(){
     return(
-        <div>
-            <br/>
-            <div className="flex p-2 flex-col items-center">
-                <div className="flex flex-row gap-[1em] md:flex md:flex-row md:gap-[1.5em]">
+        <div className="flex flex-col">
+            <p className="font-bold flex justify-center text-[2em]">Technologies</p>
+            <div className="flex p-2 flex-col overflow-x-auto no-scrollbar w-100vw">
+                <div className="animate-infinite-scroll  flex gap-5 w-max md:flex md:flex-row md:gap-[1.5em]">
 
                     <GitIcon/>
 
                     <div>
-                        <img className="w-[2.5em] md:w-[4em]" src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png"/>
+                        <img className="shrink-0 w-[8em] md:w-[4em]" src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png"/>
                     </div>      
 
-                        <VscodeIcon/>
+                    <VscodeIcon/>
                             
-                        <HtmlIcon/>
+                    <HtmlIcon/>
                             
-                        <CssIcon/>
+                    <CssIcon/>
                             
-                        <TailwindIcon/>
+                    <TailwindIcon/>
                             
-                        <JsIcon/>
+                    <JsIcon/>
                             
-                        <ReactIcon/>
+                    <ReactIcon/>
                             
-                        <NodeIcon/>
+                    <NodeIcon/>
                             
-                        <ExpressIcon/>
+                    <ExpressIcon/>
 
-                        <MongoIcon/>
+                    <MongoIcon/>
 
-                        <VercelIcon/>
+                    <VercelIcon/>
 
-                        <ViteIcon/>
+                    <ViteIcon/>
+
+                    <GitIcon/>
+
+                    <div>
+                        <img className="w-[8em] md:w-[4em]" src="https://icon.icepanel.io/Technology/png-shadow-512/GitHub.png"/>
+                    </div>      
+
+                    <VscodeIcon/>
+                            
+                    <HtmlIcon/>
+                            
+                    <CssIcon/>
+                            
+                    <TailwindIcon/>
+                            
+                    <JsIcon/>
+                            
+                    <ReactIcon/>
+                            
+                    <NodeIcon/>
+                            
+                    <ExpressIcon/>
+
+                    <MongoIcon/>
+
+                    <VercelIcon/>
+
+                    <ViteIcon/>
                 </div>
             </div>
         </div>
